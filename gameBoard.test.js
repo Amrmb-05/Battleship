7cee5e7171594b7e7386662d6e6911d0ef693266
amrmohamed <amoree.mb@gmail.com>
2023-10-21 13:08:01 +0300

    Write place shi[ function and its tests

--- gameBoard.test.js
+++ gameBoard.test.js
@@ -1,2 +1,21 @@
 import Ship from "./src";
 import Gameboard from "./src/gameBoard";
+
+let newGame;
+let ship;
+describe("places ship in the specified coordinates and direction", () => {
+  beforeEach(() => {
+    newGame = new Gameboard();
+    ship = new Ship(2);
+  });
+  test("places ship in the specified coordinates horizontally", () => {
+    newGame.placeShip(2, 2, "horizontal", 2);
+    expect(newGame.board[2][2]).toEqual(ship);
+    expect(newGame.board[1][2]).toEqual(ship);
+  });
+  test("places ship in the specified coordinates vertically", () => {
+    newGame.placeShip(2, 2, "vertical", 2);
+    expect(newGame.board[2][2]).toEqual(ship);
+    expect(newGame.board[2][3]).toEqual(ship);
+  });
+});
--- src/gameBoard.js
+++ src/gameBoard.js
@@ -2,12 +2,22 @@ import Ship from ".";
 
 export default class Gameboard {
   constructor() {
-    this.grid = [];
+    this.board = [];
     for (let i = 0; i < 10; i += 1) {
-      this.grid[i] = [];
+      this.board[i] = [];
       for (let j = 0; j < 10; j += 1) {
-        this.grid[i][j] = j;
+        this.board[i][j] = j;
       }
+    }
+  }
+
+  placeShip(x, y, direction, length) {
+    const ship = new Ship(length);
+    this.board[x][y] = ship;
+    for (let i = 0; i < length; i += 1) {
+      if (direction === "horizontal") {
+        this.board[x - i][y] = ship;
+      } else if (direction === "vertical") this.board[x][y + i] = ship;
     }
   }
 }
