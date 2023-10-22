72bf1ff6bdd5b208bd2f8cdb3a6cc0f6e4b63e7a
amrmohamed <amoree.mb@gmail.com>
2023-10-22 13:06:33 +0300

    used webpack to split js file entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    player: "./src/player.js",
+    board: "./src/gameBoard.js",
+  },
   devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
